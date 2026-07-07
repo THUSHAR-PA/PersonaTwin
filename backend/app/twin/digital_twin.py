0c30428547626cdcf2ef38bc6c7c28b94f93c7c7
Thushar P A <thusharpa@gmail.com>
2026-07-07 22:51:39 +0530

    initial commit

---
+++ backend/app/twin/digital_twin.py
@@ -0,0 +1,21 @@
+class DigitalTwin:
+
+    def __init__(self):
+        self.name = "Thushar"
+
+        self.career_score = 0
+
+        self.finance_score = 0
+
+        self.learning_score = 0
+
+        self.github_connected = False
+
+    def sync(self):
+        pass
+
+    def simulate(self):
+        pass
+
+    def predict(self):
+        pass
